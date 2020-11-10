62167ad826aa5bab6cc7b3f64b0d17996f92251b
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-10 18:55:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ outer_index = 0
 while outer_index < src.length do 
   inner_index = 0 
   while inner_index < src[outer_index].length do 
-    if src[outer_index][inner_index] % 2 
+    if src[outer_index][inner_index] % 2 == 0
       p src[outer_index][inner_index]
     end
       inner_index += 1
